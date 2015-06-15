6eb8469f37102d854ba1bc76fd4ff45a3f7a74b5
BJ Jang <jangbi882@gmail.com>
2015-06-15 15:49:21 +0900

    리팩토링 1차 완료

--- geocoding_korea.py
+++ geocoding_korea.py
@@ -30,14 +30,9 @@ from geocoding_korea_dialog import GeoCodingKoreaDialog
 import os.path
 import resources_rc
 
+
 class GeoCodingKorea:
     """QGIS Plugin Implementation."""
-    _crs_name = None
-    _address_col_name =None
-    _sim_ratio_column = "sim_ratio"
-    _service_name_column = "svc_name"
-    _request_addr_column = "req_addr"
-    _response_addr_column = "res_addr"
 
     def __init__(self, iface):
         """Constructor.
@@ -66,12 +61,11 @@ class GeoCodingKorea:
                 QCoreApplication.installTranslator(self.translator)
 
         # Create the dialog (after translation) and keep reference
-        self.dlg = GeoCodingKoreaDialog()
+        self.dlg = GeoCodingKoreaDialog(iface, iface.mainWindow())
 
         # Declare instance attributes
         self.actions = []
         self.menu = self.tr(u'&GeoCoding for Korea')
-        # TODO: We are going to let the user set this up in a future iteration
         self.toolbar = self.iface.addToolBar(u'GeoCodingKorea')
         self.toolbar.setObjectName(u'GeoCodingKorea')
 
@@ -195,65 +189,17 @@ class GeoCodingKorea:
                 or layer.geometryType() != QGis.NoGeometry:
             self.alert(
                 u'레이어 트리에서 속성 테이블을 선택후 실행해 주십시오.\n'
-                +u'속성 테이블은 엑셀 파일이나 CSV파일을 레이어 트리에 끌어다 놓아 만드실 수 있습니다.')
+                + u'속성 테이블은 엑셀 파일이나 CSV 파일을 레이어 트리에 끌어다 놓아 만드실 수 있습니다.')
             return
 
-        # 최근 사용한 좌표계 정보 채워 넣기
-        # http://gis.stackexchange.com/questions/86796/get-list-of-crs-in-qgis-by-python
-        crs_list = QSettings().value('UI/recentProjectionsAuthId')
-        self.dlg.cmbCrs.clear()
-        self.dlg.cmbCrs.addItems(crs_list)
-
-        # Project의 좌표계를 기본으로 설정
-        crs_index = 0
-        crr_crs = self.iface.mapCanvas().mapRenderer().destinationCrs().authid()
-        if not self._crs_name:
-            self._crs_name = crr_crs
-
-        for i in range(len(crs_list)):
-            crs = crs_list[i]
-            if crs == crr_crs:
-                crs_index = i
-        self.dlg.cmbCrs.setCurrentIndex(crs_index)
-
-        # 원본 주소로 사용 가능한 텍스트 컬럼 확인
-        provider = layer.dataProvider()
-        if not provider:
-            return
-
-        address_col_index = 0
-        fields = provider.fields()
-        field_name_list = []
-        for i in range(len(fields)):
-            field = fields[i]
-            field_type = field.type()
-            # 문자열 타입만 리스트에 추가
-            if field_type != QVariant.String:
-                continue
-            field_name = field.name()
-            if not self._address_col_name:
-                self._address_col_name = field_name
-            if self._address_col_name == field_name:
-                address_col_index = i
-            field_name_list.append(field_name)
-
-        # 원본 주소 컬럼 선택 가능하게 채우기
-        self.dlg.cmbAddrCol.clear()
-        self.dlg.cmbAddrCol.addItems(field_name_list)
-        self.dlg.cmbAddrCol.setCurrentIndex(address_col_index)
-
-        # 새로 만들 컬럼명 설정
-        self.dlg.ledtSimRatio.setText(self._sim_ratio_column)
-        self.dlg.ledtServiceName.setText(self._service_name_column)
-        self.dlg.ledtCleanAddr.setText(self._request_addr_column)
-        self.dlg.ledtRetAddr.setText(self._response_addr_column)
-
-        # TODO: 테이블에 컬럼과 테이터를 채워 넣기
-        self.drawDataTable(layer)
+        self.dlg.fill_crs()
+        self.dlg.fill_addr_column(layer)
+        # 테이블에 컬럼과 테이터를 채워 넣기
+        self.dlg.draw_data_table(layer)
 
         # show the dialog
         self.dlg.show()
-        self.dlg.progressBar.hide()
+        #self.dlg.progressBar.hide()
 
         # Run the dialog event loop
         result = self.dlg.exec_()
@@ -272,42 +218,3 @@ class GeoCodingKorea:
 
             pass
 
-
-    def drawDataTable(self, layer): # Called when user switches tabWidget to the Table Preview
-        provider = layer.dataProvider()
-        fields = provider.fields()
-        col_count = len(fields)
-        row_count = provider.featureCount()
-        self.dlg.dataTable.clear()
-        self.dlg.repaint()
-        self.dlg.dataTable.setColumnCount(col_count)
-        self.dlg.dataTable.setRowCount(row_count)
-        header = []
-        for i in fields:
-          header.append(i.name())
-        self.dlg.dataTable.setHorizontalHeaderLabels(header)
-        self.dlg.progressBar.setRange (0, row_count+1)
-        self.dlg.progressBar.setFormat(self.tr('Drawing table') +': %p%')
-        formatting = False
-        if formatting: # slower procedure, with formatting the table items
-          """
-          for i in range(len(self.data)):
-            self.progressBar.setValue(i+1)
-            for j in range(len(self.data[i])):
-              item = QTableWidgetItem(unicode(self.data[i][j] or 'NULL'))
-              item.setFlags(Qt.ItemIsSelectable)
-              if fields[i].type() == 6 or fields[i].type() == 2:
-                item.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
-              self.dataTable.setItem(j,i,item)
-          """
-        else: # about 25% faster procedure, without formatting
-          i = 0
-          for f in layer.getFeatures():
-              self.dlg.progressBar.setValue(i+1)
-              for j in range(col_count):
-                  val = f[j]
-                  self.dlg.dataTable.setItem(i,j,QTableWidgetItem(unicode(val or 'NULL')))
-              i += 1
-        self.dlg.dataTable.resizeColumnsToContents()
-        self.needsRedraw = False
-        self.dlg.progressBar.reset()
--- geocoding_korea_dialog.py
+++ geocoding_korea_dialog.py
@@ -22,7 +22,8 @@
 """
 
 import os
-
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 from PyQt4 import QtGui, uic
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
@@ -30,7 +31,15 @@ FORM_CLASS, _ = uic.loadUiType(os.path.join(
 
 
 class GeoCodingKoreaDialog(QtGui.QDialog, FORM_CLASS):
-    def __init__(self, parent=None):
+    # Member variable
+    _crs_name = None
+    _address_col_name = None
+    _sim_ratio_column = "sim_ratio"
+    _service_name_column = "svc_name"
+    _request_addr_column = "req_addr"
+    _response_addr_column = "res_addr"
+
+    def __init__(self, iface, parent=None):
         """Constructor."""
         super(GeoCodingKoreaDialog, self).__init__(parent)
         # Set up the user interface from Designer.
@@ -38,4 +47,90 @@ class GeoCodingKoreaDialog(QtGui.QDialog, FORM_CLASS):
         # self.<objectname>, and you can use autoconnect slots - see
         # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
         # #widgets-and-dialogs-with-auto-connect
+        self.iface = iface
         self.setupUi(self)
+        self.ledtSimRatio.setText(self._sim_ratio_column)
+        self.ledtServiceName.setText(self._service_name_column)
+        self.ledtCleanAddr.setText(self._request_addr_column)
+        self.ledtRetAddr.setText(self._response_addr_column)
+
+    # 좌표계 정보 채우기
+    def fill_crs(self):
+        # 최근 사용한 좌표계 정보 채워 넣기. 좌표계 전체를 다 뿌리면 너무 많다!
+        # http://gis.stackexchange.com/questions/86796/get-list-of-crs-in-qgis-by-python
+        crs_list = QSettings().value('UI/recentProjectionsAuthId')
+        self.cmbCrs.clear()
+        self.cmbCrs.addItems(crs_list)
+
+        # Project의 좌표계를 기본으로 설정
+        crs_index = 0
+        crr_crs = self.iface.mapCanvas().mapRenderer().destinationCrs().authid()
+        if not self._crs_name:
+            self._crs_name = crr_crs
+
+        for i in range(len(crs_list)):
+            crs = crs_list[i]
+            if crs == crr_crs:
+                crs_index = i
+        self.cmbCrs.setCurrentIndex(crs_index)
+
+    # 원본 주소로 사용 가능한 텍스트 컬럼 확인
+    def fill_addr_column(self, layer):
+        provider = layer.dataProvider()
+        if not provider:
+            return
+
+        address_col_index = 0
+        fields = provider.fields()
+        field_name_list = []
+        for i in range(len(fields)):
+            field = fields[i]
+            field_type = field.type()
+            # 문자열 타입만 리스트에 추가
+            if field_type != QVariant.String:
+                continue
+            field_name = field.name()
+            if not self._address_col_name:
+                self._address_col_name = field_name
+            if self._address_col_name == field_name:
+                address_col_index = i
+            field_name_list.append(field_name)
+
+        # 원본 주소 컬럼 선택 가능하게 채우기
+        self.cmbAddrCol.clear()
+        self.cmbAddrCol.addItems(field_name_list)
+        self.cmbAddrCol.setCurrentIndex(address_col_index)
+
+    def draw_data_table(self, layer):
+        provider = layer.dataProvider()
+        fields = provider.fields()
+        col_count = len(fields)
+        row_count = provider.featureCount()
+        self.dataTable.clear()
+        self.repaint()
+        self.dataTable.setColumnCount(col_count)
+        self.dataTable.setRowCount(row_count)
+        header = []
+        for i in fields:
+            header.append(i.name())
+        self.dataTable.setHorizontalHeaderLabels(header)
+        self.progressBar.setRange(0, row_count+1)
+        self.progressBar.setFormat(self.tr('Drawing table') + ': %p%')
+
+        if row_count <= 200:
+            formatting = True
+        else:
+            formatting = False
+        i = 0
+        for f in layer.getFeatures():
+            self.progressBar.setValue(i+1)
+            for j in range(col_count):
+                val = f[j]
+                item = QTableWidgetItem(unicode(val or 'NULL'))
+                item.setFlags(Qt.ItemIsSelectable)
+                if formatting and (fields[j].type() == 6 or fields[j].type() == 2):
+                    item.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
+                self.dataTable.setItem(i, j, item)
+            i += 1
+        self.dataTable.resizeColumnsToContents()
+        self.progressBar.reset()
